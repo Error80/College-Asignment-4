1e3d563822c94fd2c22ee99981af38a8963320ac
Error80 <122498961+Error80@users.noreply.github.com>
2024-03-25 22:02:39 -0400

    Edited JS

--- Part 2/main.js
+++ Part 2/main.js
@@ -5,14 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageList = ["images/pic1.jpg","images/pic2.jpg","images/pig3.jpg","images/pic4.jpg","images/pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+const imageAltText = ["A baby's eye", "A rock wall", "Purple Flowers", "Hieroglyphs from Ancient Egypt","A Moth"]
 
 /* Looping through images */
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', imageList[0]);
+newImage.setAttribute('alt', imageAltText[0]);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
